5e4fa72e68ca0113ebb558c611af16b354b27804
Kaustubh <kaustubhkushte@gmail.com>
2016-02-20 21:10:46 +0530

    Location Search Implemented

--- BloodNet/www/js/controllers/LandingController.js
+++ BloodNet/www/js/controllers/LandingController.js
@@ -1,8 +1,111 @@
 controllers.controller('LandingController',['$scope',
 'AppHelperFactory',
 'AppConstants',
-function($scope,helper,constants){
+'$cordovaDatePicker',
+'$ionicModal',
+function($scope,helper,constants,datePicker,$ionicModal){
 
   helper.log('Landing Loaded');
-  
+
+  $scope.mobileNumberRegEx="/^[0-9]{10,10}$/;"
+
+  $scope.register = {};
+  $scope.register.wholeBloodDate = '';
+  $scope.register.platelatesDate = '';
+  $scope.register.plasmaDate = '';
+  $scope.register.preferredLocationName = '';
+  $scope.register.preferredLocationLat = 77.33;
+  $scope.register.preferredLocationLong = 28.22;
+
+  //TODO : Move blood list to a data folder
+  $scope.locations = [
+      {"title":"BTM","lat":12,"long":232},
+      {"title":"BTM","lat":12,"long":232}
+  ];
+  $scope.bloodGroupList = [
+{"title":"AB+","value":1},
+{"title":"AB-","value":2},
+{"title":"A+","value":3},
+{"title":"A-","value":4},
+{"title":"B+","value":5},
+{"title":"B-","value":6},
+{"title":"O+","value":7},
+{"title":"O-","value":8}
+];
+
+
+  $ionicModal.fromTemplateUrl('templates/preffered-location-search.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal
+  });
+
+  $scope.showLocationSearchModal = function() {
+    $scope.curSelectedLocation = undefined;
+    $scope.modal.show();
+  }
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  $scope.cancelLocationSearch = function()
+  {
+    $scope.modal.hide();
+  }
+
+
+  $scope.submitDetails = function()
+  {
+    alert(1);
+  }
+
+
+
+  $scope.selectedLocation = function(location)
+  {
+    $scope.curSelectedLocation = location;
+    helper.debug(location);
+  }
+
+  $scope.doneSelection = function()
+  {
+    if ($scope.curSelectedLocation != undefined) {
+      $scope.register.preferredLocationName = $scope.curSelectedLocation.title;
+      $scope.register.preferredLocationLat = $scope.curSelectedLocation.lat;
+      $scope.register.preferredLocationLong = $scope.curSelectedLocation.long;
+    }
+    $scope.closeModal();
+  }
+
+
+  $scope.showDOBPicker = function()
+  {
+    var datePickerOptions = {
+      date: new Date(),
+      mode: 'date', // or 'time'
+      minDate: new Date('1945','01','01'),
+      allowOldDates: true,
+      allowFutureDates: false,
+      doneButtonLabel: 'DONE',
+      doneButtonColor: '#000000',
+      cancelButtonLabel: 'CANCEL',
+      cancelButtonColor: '#000000'
+    };
+    datePicker.show(datePickerOptions).then(function(date){
+        if (date !== undefined) {
+          var month = date.getMonth();
+          var day = date.getDay();
+          var year = date.getFullYear();
+          $scope.register.dob = year +'/'+day+'/'+month;
+        }
+    });
+  }
+
+
 }]);
--- BloodNet/www/js/directives/directives.js
+++ BloodNet/www/js/directives/directives.js
@@ -3,3 +3,22 @@ app.directive('appHeader', function() {
     template: '<img src="img/header-logo.png" class="header-logo" />'
   };
 });
+
+app.directive('numericOnly', function(){
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, modelCtrl) {
+
+            modelCtrl.$parsers.push(function (inputValue) {
+                var transformedInput = inputValue ? inputValue.replace(/[^\d.-]/g,'') : null;
+
+                if (transformedInput!=inputValue) {
+                    modelCtrl.$setViewValue(transformedInput);
+                    modelCtrl.$render();
+                }
+
+                return transformedInput;
+            });
+        }
+		}
+});
